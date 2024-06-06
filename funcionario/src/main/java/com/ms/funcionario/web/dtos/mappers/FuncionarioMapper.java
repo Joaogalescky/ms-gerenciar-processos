dc5937fddb1d5a2829a0f8cfe36c64b5fb4246b4
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-06-06 12:30:27 -0300

    Refactor: alterando a logica do mapper para usar o dto FuncionarioListarDto

--- funcionario/src/main/java/com/ms/funcionario/web/dtos/mappers/FuncionarioMapper.java
+++ funcionario/src/main/java/com/ms/funcionario/web/dtos/mappers/FuncionarioMapper.java
@@ -1,17 +1,26 @@
 package com.ms.funcionario.web.dtos.mappers;
 
 import com.ms.funcionario.entities.Funcionario;
-import com.ms.funcionario.web.dtos.FuncionarioResponseDto;
+import com.ms.funcionario.web.dtos.FuncionarioCadastrarDto;
+import com.ms.funcionario.web.dtos.FuncionarioListarDto;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FuncionarioMapper {
-    public static Funcionario toFuncionario(FuncionarioResponseDto createDto) {
+    public static Funcionario toFuncionarioCadastrar(FuncionarioCadastrarDto createDto) {
         return new ModelMapper().map(createDto, Funcionario.class);
     }
 
-    public static FuncionarioResponseDto toDto(Funcionario funcionario) {
-        return new ModelMapper().map(funcionario, FuncionarioResponseDto.class);
+    public static FuncionarioCadastrarDto toDtoCadastrar(Funcionario funcionario) {
+        return new ModelMapper().map(funcionario, FuncionarioCadastrarDto.class);
     }
+
+    public static Funcionario toFuncionarioListar(FuncionarioListarDto funcionarioListarDto) {
+        return new ModelMapper().map(funcionarioListarDto, Funcionario.class);
+    }
+    public static FuncionarioListarDto toListDto(Funcionario funcionario) {
+        return new ModelMapper().map(funcionario, FuncionarioListarDto.class);
+    }
+
 }
