3036665828d54aad7923b173e7148a77303dfb1c
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-06-06 08:00:18 -0300

    Criado Método buscarPorId em PropostaService

--- proposta/src/main/java/com/ms/proposta/services/PropostaService.java
+++ proposta/src/main/java/com/ms/proposta/services/PropostaService.java
@@ -1,11 +1,13 @@
 package com.ms.proposta.services;
 
 import com.ms.proposta.entities.Proposta;
+import com.ms.proposta.exceptions.IdPropostaNaoEncontradoException;
 import com.ms.proposta.repositories.PropostaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -14,12 +16,20 @@ public class PropostaService {
     @Autowired
     private final PropostaRepository propostaRepository;
 
-    public Proposta cadastrarProposta (Proposta proposta) {
+    public Proposta cadastrarProposta(Proposta proposta) {
+        proposta.setDataProposta(new Date());
         return propostaRepository.save(proposta);
     }
 
-    public List <Proposta> listarPropostas() {
+    public List<Proposta> listarPropostas() {
         return propostaRepository.findAll();
     }
+
+    public Proposta buscarPorId(Long id) {
+        return propostaRepository.findById(id).orElseThrow(
+                () -> new IdPropostaNaoEncontradoException(String.format("Proposta id=%s não encontrada", id))
+        );
+    }
+
 
 }
