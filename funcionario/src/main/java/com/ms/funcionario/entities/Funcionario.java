8fbf192180b210896afa6d5670ebc725dee70cb0
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-06-06 12:24:25 -0300

    Refactor: alterando tipo das variaveis dataNasc e sexo

--- funcionario/src/main/java/com/ms/funcionario/entities/Funcionario.java
+++ funcionario/src/main/java/com/ms/funcionario/entities/Funcionario.java
@@ -2,6 +2,7 @@ package com.ms.funcionario.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -9,19 +10,19 @@ import java.util.Date;
 @Table(name = "funcionario")
 @Entity(name = "funcionario")
 @Data
+@NoArgsConstructor
 public class Funcionario implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
+    @Column(name = "id", nullable = false, unique = true)
     private Long id;
     @Column(name = "nome", nullable = false, length = 200)
     private String nome;
     @Column(name = "data_nascimento", nullable = false)
-    @Temporal(TemporalType.DATE)
-    private Date dataNasc;
-    @Column(name = "cpf", nullable = false, length = 11) // 111 111 111 11
+    private String dataNasc;
+    @Column(name = "cpf", nullable = false, length = 11, unique = true) // 111 111 111 11
     private String cpf;
     @Column(name = "sexo", nullable = false)
-    private Enum sexo;
+    private String sexo;
 }
