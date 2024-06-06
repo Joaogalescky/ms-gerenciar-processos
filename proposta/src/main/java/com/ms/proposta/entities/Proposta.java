ee4825aed10b6d06aad0bb49a5d58b7c9c7898f6
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-06-06 07:47:53 -0300

    Criada Entidade Proposta

---
+++ proposta/src/main/java/com/ms/proposta/entities/Proposta.java
@@ -0,0 +1,63 @@
+package com.ms.proposta.entities;
+
+import com.ms.proposta.Enum.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "propostas")
+public class Proposta implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+    @Column(name = "descricao", nullable = false, length = 500)
+    private String descricao;
+    @Column(name = "dataProposta")
+    private Date dataProposta;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private Status Status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Proposta proposta)) return false;
+        return Objects.equals(getId(), proposta.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Proposta{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", dataProposta=" + dataProposta +
+                ", Status=" + Status +
+                '}';
+    }
+
+}
