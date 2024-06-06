0919fdb709494b99e78affc894975a9650861742
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-06-06 12:29:36 -0300

    Refactor: alterando a logica do cadastrarFuncionario e criando logica do alterarFuncionario e deletarFuncionario

--- funcionario/src/main/java/com/ms/funcionario/services/FuncionarioService.java
+++ funcionario/src/main/java/com/ms/funcionario/services/FuncionarioService.java
@@ -2,7 +2,11 @@ package com.ms.funcionario.services;
 
 import com.ms.funcionario.entities.Funcionario;
 import com.ms.funcionario.repositories.FuncionarioRepository;
-import com.ms.funcionario.web.exceptions.FuncionarioIdNaoEncontrado;
+import com.ms.funcionario.web.dtos.FuncionarioCadastrarDto;
+import com.ms.funcionario.web.dtos.FuncionarioListarDto;
+import com.ms.funcionario.web.dtos.mappers.FuncionarioMapper;
+import com.ms.funcionario.exceptions.FuncionarioIdNaoEncontrado;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +15,16 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class FuncionarioService {
+
     private final FuncionarioRepository funcionarioRepository;
 
     public List<Funcionario> listarFuncionario() {
         return funcionarioRepository.findAll();
     }
 
-    public Funcionario cadastrarFuncionario(Funcionario funcionario) {
+    @Transactional
+    public Funcionario cadastrarFuncionario(FuncionarioCadastrarDto funcionarioCadastrarDto) {
+        Funcionario funcionario = FuncionarioMapper.toFuncionarioCadastrar(funcionarioCadastrarDto);
         return funcionarioRepository.save(funcionario);
     }
 
@@ -25,5 +32,19 @@ public class FuncionarioService {
         return funcionarioRepository.findById(id).orElseThrow(
                 () -> new FuncionarioIdNaoEncontrado(String.format("Funcionario id=%s não encontrado", id))
         );
+    }
+    @Transactional
+    public Funcionario alterarFuncionario(Long idFunc, FuncionarioListarDto funcionarioListarDto) {
+      Funcionario funcionario = buscarPorId(idFunc);
+      funcionario.setNome(funcionarioListarDto.getNome());
+      funcionario.setDataNasc(funcionarioListarDto.getDataNasc());
+      funcionario.setCpf(funcionarioListarDto.getCpf());
+      funcionario.setSexo(funcionarioListarDto.getSexo());
+      return funcionarioRepository.save(funcionario);
+    }
+
+    public void deletarFuncionario(Long id){
+        Funcionario funcionario = buscarPorId(id);
+       funcionarioRepository.delete(funcionario);
     }
 }
