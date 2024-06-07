6a71a2f1994b304a2c8f1b2f6e7e1559d093daa7
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-07 14:05:11 -0300

    Refactor: alterando dataNasc para Date

--- funcionario/src/main/java/com/ms/funcionario/web/dtos/FuncionarioListarDto.java
+++ funcionario/src/main/java/com/ms/funcionario/web/dtos/FuncionarioListarDto.java
@@ -2,11 +2,12 @@ package com.ms.funcionario.web.dtos;
 
 import lombok.Data;
 
+import java.util.Date;
 
 @Data
 public class FuncionarioListarDto {
     private String nome;
-    private String dataNasc;
+    private Date dataNasc;
     private String cpf;
     private String sexo;
 }
