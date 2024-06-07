6f622d870d05c5dd77d714b25732101c0703ed75
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-07 14:04:43 -0300

    Refactor: removendo exception não utilizada

--- funcionario/src/main/java/com/ms/funcionario/exceptions/FuncionarioIdNaoEncontrado.java
+++
@@ -1,7 +0,0 @@
-package com.ms.funcionario.exceptions;
-
-public class FuncionarioIdNaoEncontrado extends RuntimeException {
-    public FuncionarioIdNaoEncontrado(String message) {
-        super(message);
-    }
-}
