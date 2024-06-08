67623fc6716497e15a39840390b9835491d6618d
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-06-08 17:40:24 -0300

    Entidade Voto e seus métodos implementados

--- resultado/src/main/java/com/ms/resultado/services/VotoService.java
+++ resultado/src/main/java/com/ms/resultado/services/VotoService.java
@@ -1,11 +1,17 @@
 package com.ms.resultado.services;
 
+import com.ms.resultado.entities.SessaoVotacao;
 import com.ms.resultado.entities.Voto;
+import com.ms.resultado.enums.StatusSessao;
+import com.ms.resultado.repositories.SessaoVotacaoRepository;
 import com.ms.resultado.repositories.VotoRepository;
+import com.ms.resultado.repositories.FuncionarioClient;
 import com.ms.resultado.web.dtos.VotoCadastroDto;
 import com.ms.resultado.web.dtos.mappers.VotoMapper;
+import com.ms.resultado.web.exceptions.EntidadeNaoEncontradaException;
+import com.ms.resultado.web.exceptions.VotoDuplicadoException;
+import com.ms.resultado.web.exceptions.PropostaNaoEncontradaException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,13 +19,43 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class VotoService {
 
-    @Autowired
     private final VotoRepository votoRepository;
+    private final FuncionarioClient funcionarioClient;
+    private final SessaoVotacaoRepository sessaoVotacaoRepository;
 
     @Transactional
-    public Voto votoCadastrar(VotoCadastroDto votoCadastroDto) {
+    public Voto cadastrarVoto(VotoCadastroDto votoCadastroDto) {
+        verificarEntidadesExistentes(votoCadastroDto.getIdSessao(), votoCadastroDto.getIdFunc());
+
+        if (verificarVotoExistente(votoCadastroDto.getIdSessao(), votoCadastroDto.getIdFunc())) {
+            throw new VotoDuplicadoException("Este funcionário já votou nesta sessão de votação.");
+        }
+
         Voto voto = VotoMapper.toVotoCadastrar(votoCadastroDto);
         return votoRepository.save(voto);
     }
 
+    private void verificarEntidadesExistentes(Long sessaoVotacaoId, Long funcionarioId) {
+        try {
+            SessaoVotacao sessaoVotacao = sessaoVotacaoRepository.findById(sessaoVotacaoId).orElseThrow(() -> new EntidadeNaoEncontradaException("Sessão de votação não encontrada."));
+            if (sessaoVotacao.getStatus() == StatusSessao.INATIVO) {
+                throw new PropostaNaoEncontradaException("Não é possível votar em uma sessão de votação INATIVA");
+            }
+        } catch (PropostaNaoEncontradaException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new EntidadeNaoEncontradaException("Erro ao verificar a sessão de votação.");
+        }
+
+        try {
+            funcionarioClient.buscarPorId(funcionarioId);
+        } catch (Exception e) {
+            throw new EntidadeNaoEncontradaException("Funcionário não encontrado.");
+        }
+    }
+
+
+    public boolean verificarVotoExistente(Long sessaoId, Long funcionarioId) {
+        return votoRepository.existsByIdSessaoAndIdFunc(sessaoId, funcionarioId);
+    }
 }
--- resultado/src/main/java/com/ms/resultado/web/controllers/VotoController.java
+++ resultado/src/main/java/com/ms/resultado/web/controllers/VotoController.java
@@ -1,25 +1,24 @@
 package com.ms.resultado.web.controllers;
 
 import com.ms.resultado.entities.Voto;
-
 import com.ms.resultado.services.VotoService;
 import com.ms.resultado.web.dtos.VotoCadastroDto;
 import com.ms.resultado.web.dtos.mappers.VotoMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/votos")
+@RequestMapping("/api/v1/votos")
+@RequiredArgsConstructor
 public class VotoController {
 
-    @Autowired
-    private VotoService votoService;
+    private final VotoService votoService;
 
     @PostMapping
     public ResponseEntity<VotoCadastroDto> registrarVoto(@RequestBody VotoCadastroDto votoCadastroDto) {
-        Voto voto = votoService.votoCadastrar(votoCadastroDto);
+        Voto voto = votoService.cadastrarVoto(votoCadastroDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(VotoMapper.toVotoCadastroDto(voto));
     }
 }
