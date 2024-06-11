99e8203cbc7b9a4acc818144fd6b6aec8a685d0c
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-10 22:54:42 -0300

    Refactor: alteração de nome DadosFuncionarioInvalidos para DadosFuncionarioInvalidosException

--- funcionario/src/main/java/com/ms/funcionario/exceptions/DadosFuncionarioInvalidos.java
+++
@@ -1,7 +0,0 @@
-package com.ms.funcionario.exceptions;
-
-public class DadosFuncionarioInvalidos extends RuntimeException {
-    public DadosFuncionarioInvalidos(String message) {
-        super(message);
-    }
-}
