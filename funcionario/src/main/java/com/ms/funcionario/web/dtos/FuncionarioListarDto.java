4b567c051adf70a17d320c4fe46ae2ddd060d0f0
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-06-11 10:47:48 -0300

    Implementados testes unitários do FuncionarioService

--- funcionario/src/main/java/com/ms/funcionario/web/dtos/FuncionarioListarDto.java
+++ funcionario/src/main/java/com/ms/funcionario/web/dtos/FuncionarioListarDto.java
@@ -1,13 +1,20 @@
 package com.ms.funcionario.web.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@AllArgsConstructor
 public class FuncionarioListarDto {
     private String nome;
-    private Date dataNasc;
+    private String dataNasc;
     private String cpf;
     private String sexo;
+
+    public FuncionarioListarDto(){
+
+    }
 }
--- funcionario/src/test/java/com/ms/funcionario/common/FuncionarioConstants.java
+++
@@ -1,7 +0,0 @@
-package com.ms.funcionario.common;
-
-import com.ms.funcionario.entities.Funcionario;
-
-public class FuncionarioConstants {
-    public static final Funcionario FUNCIONARIO = new Funcionario("id", "nome", "dataNasc", "cpf", "sexo");
-}
