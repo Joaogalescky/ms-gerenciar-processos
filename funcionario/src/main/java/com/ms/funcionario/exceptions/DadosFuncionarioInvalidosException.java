5426be379d1e8565dcb5cbddc5be0a2dd7dc1410
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-11 10:12:20 -0300

    Refactor: DadosFuncionarioInvalidosException, parametro 'message' para 'mensagem'

--- funcionario/src/main/java/com/ms/funcionario/exceptions/DadosFuncionarioInvalidosException.java
+++ funcionario/src/main/java/com/ms/funcionario/exceptions/DadosFuncionarioInvalidosException.java
@@ -1,7 +1,7 @@
 package com.ms.funcionario.exceptions;
 
 public class DadosFuncionarioInvalidosException extends RuntimeException {
-    public DadosFuncionarioInvalidosException(String message) {
-        super(message);
+    public DadosFuncionarioInvalidosException(String mensagem) {
+        super(mensagem);
     }
 }
