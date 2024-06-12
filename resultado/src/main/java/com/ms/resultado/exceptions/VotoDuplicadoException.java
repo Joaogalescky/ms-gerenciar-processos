9230285d054c1e6404ccb73ab6137534934dd061
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-11 22:44:03 -0300

    Refactor: VotoDuplicadoException - micro servico resultado

--- resultado/src/main/java/com/ms/resultado/exceptions/VotoDuplicadoException.java
+++ resultado/src/main/java/com/ms/resultado/exceptions/VotoDuplicadoException.java
@@ -1,6 +1,6 @@
 package com.ms.resultado.exceptions;
 
-public class VotoDuplicadoException extends RuntimeException{
+public class VotoDuplicadoException extends RuntimeException {
     public VotoDuplicadoException(String message) {
         super(message);
     }
