f858ed3b3edabf289906474dd4c6cc102c89714f
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-11 23:00:41 -0300

    Refactor: PropostaController - micro servico proposta

--- proposta/src/main/java/com/ms/proposta/web/controllers/PropostaController.java
+++ proposta/src/main/java/com/ms/proposta/web/controllers/PropostaController.java
@@ -20,9 +20,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Tag(name = "Propostas", description ="Contém todas as operações relativas aos para cadastro, edição e leitura de uma proposta.")
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/propostas")
-@RequiredArgsConstructor
 public class PropostaController {
 
     @Autowired
@@ -30,14 +30,26 @@ public class PropostaController {
 
     private final FuncionarioClient funcionarioClient;
 
-    @Operation(summary = "Cadastrar uma nova proposta", description = "Recurso para cadastrar uma nova proposta",
+    @Operation(
+            summary = "Cadastrar proposta",
+            description = "Recurso para cadastrar proposta",
             responses = {
-            @ApiResponse(responseCode = "201", description = "Proposta cadastrada com sucessso",
-                        content = @Content(mediaType = "application/json", schema = @Schema(implementation = PropostaCadastroDto.class))),
-                    @ApiResponse(responseCode = "409", description = "Proposta ja cadastrada no sistema",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
-                    @ApiResponse(responseCode = "422", description = "Recurso não processado por dados de entrada inválidos",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Recurso cadastrado com sucesso!",
+                            content = @Content(
+                                    mediaType = "application/json", schema = @Schema(implementation = PropostaCadastroDto.class))),
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "Recurso já cadastrado no sistema",
+                            content = @Content(
+                                    mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(
+                            responseCode = "422",
+                            description = "Recurso não processado por dados de entrada inválidos",
+                            content = @Content(
+                                    mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))
+                    )
             }
     )
     @PostMapping
@@ -46,10 +58,17 @@ public class PropostaController {
         return ResponseEntity.ok(proposta);
     }
 
-    @Operation(summary = "Listar Propostas", description = "Listar Propostas",
+    @Operation(
+            summary = "Listar propostas",
+            description = "Recurso para listar propostas",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "Proposta listada com sucessso",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = PropostaCadastroDto.class)))
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Recurso listado com sucesso!",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = PropostaCadastroDto.class))
+                    )
             }
     )
     @GetMapping
@@ -58,12 +77,19 @@ public class PropostaController {
         return ResponseEntity.ok(propostas);
     }
 
-    @Operation(summary = "Recuperar uma proposta pelo id", description = "Recuperar uma proposta pelo  id",
+    @Operation(
+            summary = "Buscar proposta",
+            description = "Recurso para buscar proposta por ID",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "Proposta recuperada com sucessso",
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Recurso buscado com sucesso!",
                             content = @Content(mediaType = "application/json", schema = @Schema(implementation = PropostaCadastroDto.class))),
-                    @ApiResponse(responseCode = "404", description = "Recurso não encontrado",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Recurso não encontrado",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))
+                    )
             }
     )
     @GetMapping("/{id}")
@@ -72,14 +98,26 @@ public class PropostaController {
         return ResponseEntity.ok(proposta);
     }
 
-    @Operation(summary = "Atualizar proposta", description = "Atualizar proposta",
+    @Operation(
+            summary = "Atualizar proposta",
+            description = "Recurso para atualizar proposta",
             responses = {
-                    @ApiResponse(responseCode = "204", description = "Proposta atualizada com sucessso",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class))),
-                    @ApiResponse(responseCode = "400", description = "Proposta não confere",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
-                    @ApiResponse(responseCode = "404", description = "Recurso não encontrado",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Recurso atualizada com sucesso!",
+                            content = @Content(
+                                    mediaType = "application/json", schema = @Schema(implementation = Void.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Proposta não confere",
+                            content = @Content(
+                                    mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Recurso não encontrado",
+                            content = @Content(
+                                    mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))
+                    )
             }
     )
     @PutMapping("/{id}")
@@ -88,12 +126,13 @@ public class PropostaController {
         return ResponseEntity.ok(proposta);
     }
 
-    @Operation(summary = "Deletar proposta", description = "Deletar proposta",
+    @Operation(summary = "Deletar proposta", description = "Recurso para deletar proposta",
             responses = {
-                    @ApiResponse(responseCode = "204", description = "Proposta deletada com sucessso",
+                    @ApiResponse(responseCode = "204", description = "Recurso deletada com sucesso!",
                             content = @Content(mediaType = "application/json", schema = @Schema(implementation = Void.class))),
                     @ApiResponse(responseCode = "404", description = "Recurso não encontrado",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))
+                    )
             }
     )
     @DeleteMapping("/{id}")
@@ -102,12 +141,13 @@ public class PropostaController {
         return ResponseEntity.noContent().build();
     }
 
-    @Operation(summary = "Recuperar um funcionario pelo id", description = "Recuperar um funcionario pelo id",
+    @Operation(summary = "Buscar funcionário", description = "Recurso para buscar funcionário por ID",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "Funcionario recuperado com sucessso",
+                    @ApiResponse(responseCode = "200", description = "Recurso buscado com sucesso!",
                             content = @Content(mediaType = "application/json", schema = @Schema(implementation = PropostaCadastroDto.class))),
                     @ApiResponse(responseCode = "404", description = "Recurso não encontrado",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))
+                    )
             }
     )
     @GetMapping("/funcionarios/{id}")
