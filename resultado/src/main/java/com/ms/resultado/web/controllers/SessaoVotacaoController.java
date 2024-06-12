1c28a1c0342f0d4d398e2560e1e2d6977f2d293e
Joaogalescky <joaocvgalescky@gmail.com>
2024-06-11 23:01:42 -0300

    Refactor: SessaoVotacaoController - micro servico resultado

--- resultado/src/main/java/com/ms/resultado/web/controllers/SessaoVotacaoController.java
+++ resultado/src/main/java/com/ms/resultado/web/controllers/SessaoVotacaoController.java
@@ -13,7 +13,8 @@ import org.springdoc.api.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-@Tag(name = "Sessão Votação", description ="Contém as operações relativas para iniciar a votação e leitura da sessão")
+
+@Tag(name = "Sessão Votação", description = "Contém as operações relativas para iniciar a votação e leitura da sessão")
 @RestController
 @RequestMapping("/api/v1/sessaovotacao")
 public class SessaoVotacaoController {
@@ -23,12 +24,26 @@ public class SessaoVotacaoController {
     public SessaoVotacaoController(SessaoVotacaoService sessaoVotacaoService) {
         this.sessaoVotacaoService = sessaoVotacaoService;
     }
-    @Operation(summary = "Iniciar votação", description = "Iniciar votação",
+
+    @Operation(summary = "Iniciar votação", description = "Recurso para iniciar votação",
             responses = {
-                    @ApiResponse(responseCode = "201", description = "Votação iniciada com sucessso",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Resultado.class))),
-                    @ApiResponse(responseCode = "409", description = "Votação ja está iniciada",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Recurso iniciado com sucesso!",
+                            content = @Content(
+                                    mediaType = "application/json", schema = @Schema(implementation = Resultado.class))),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Recurso não encontrado",
+                            content = @Content(
+                                    mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))
+                    )
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "Recurso já iniciado",
+                            content = @Content(
+                                    mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))
+                    )
             }
     )
     @PostMapping
@@ -37,12 +52,19 @@ public class SessaoVotacaoController {
         return new ResponseEntity<>(sessaoVotacao, HttpStatus.CREATED);
     }
 
-    @Operation(summary = "Buscar Sessão", description = "Buscar Sessão",
+    @Operation(summary = "Buscar sessão", description = "Recurso para buscar sessão por ID",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "Sessão buscada com sucessso",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Resultado.class))),
-                    @ApiResponse(responseCode = "404", description = "Sessão não encontrada",
-                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Recurso buscado com sucesso!",
+                            content = @Content(
+                                    mediaType = "application/json", schema = @Schema(implementation = Resultado.class))),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Recurso não encontrado",
+                            content = @Content(
+                                    mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))
+                    )
             }
     )
     @GetMapping("/{id}")
