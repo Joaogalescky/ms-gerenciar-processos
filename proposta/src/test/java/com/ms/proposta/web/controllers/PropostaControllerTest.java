8bfa63438f5259afec97410113343c93e084601d
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-06-12 09:05:20 -0300

    Implementação do teste de integração para listarProposta

--- proposta/src/test/java/com/ms/proposta/web/controllers/PropostaControllerTest.java
+++ proposta/src/test/java/com/ms/proposta/web/controllers/PropostaControllerTest.java
@@ -3,6 +3,7 @@ package com.ms.proposta.web.controllers;
 import com.ms.proposta.entities.Proposta;
 import com.ms.proposta.services.PropostaService;
 import com.ms.proposta.web.dtos.FuncionarioDto;
+import com.ms.proposta.web.dtos.PropostaAtualizacaoDto;
 import com.ms.proposta.web.dtos.PropostaCadastroDto;
 import com.ms.proposta.web.dtos.PropostaConsultaDto;
 import org.junit.jupiter.api.Assertions;
@@ -47,6 +48,27 @@ class PropostaControllerTest {
 
         Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
         Mockito.verify(propostaService, Mockito.times(1)).cadastrarProposta(Mockito.any(PropostaCadastroDto.class));
+    }
+    @Test
+    void listarPropostas(){
+        Proposta proposta1 = new Proposta();
+        proposta1.setIdFuncionario(1L);
+        proposta1.setNome("Arrumar mesa");
+        proposta1.setDescricao("arrumar mesa sala 1");
+        proposta1.setDataProposta(new Date());
+
+        Proposta proposta2 = new Proposta();
+        proposta2.setIdFuncionario(1L);
+        proposta2.setNome("Arrumar mesa");
+        proposta2.setDescricao("arrumar mesa sala 2");
+        proposta2.setDataProposta(new Date());
+
+        List<Proposta> propostas = List.of(proposta1, proposta2);
+
+        ResponseEntity<List> response = restTemplate.getForEntity("/api/v1/propostas", List.class);
+
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Mockito.verify(propostaService, Mockito.times(1)).listarPropostas();
     }
 
 
